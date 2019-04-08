2d327c34438b5ff2c4293b937509102d7f873e0f
Corbin Fox <corbin.fox@gmail.com>
2019-04-08 15:00:09 -0600

    WIP: adding ravencoin support

---
+++ bchain/coins/ravencoin/ravencoinrpc.go
@@ -0,0 +1,57 @@
+package ravencoin
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/json"
+
+	"github.com/golang/glog"
+)
+
+// ravencoinRPC is an interface to JSON-RPC bitcoind service.
+type ravencoinRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewravencoinRPC returns new ravencoinRPC instance.
+func NewravencoinRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &ravencoinRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV2{}
+	s.ChainConfig.SupportsEstimateFee = false
+
+	return s, nil
+}
+
+// Initialize initializes ravencoinRPC instance.
+func (b *ravencoinRPC) Initialize() error {
+	chainName, err := b.GetChainInfoAndInitializeMempool(b)
+	if err != nil {
+		return err
+	}
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewravencoinParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
